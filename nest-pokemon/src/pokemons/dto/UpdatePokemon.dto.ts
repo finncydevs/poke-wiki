eaf6bcbdf14081ec2e53ff2a36b0d3fa0251124c
finn <finnnxptrix@gmail.com>
2025-03-23 12:49:40 +0700

    delete

--- nest-pokemon/src/pokemons/dto/UpdatePokemon.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePokemonDto } from './CreatePokemon.dto';
-
-export class UpdatePokemonDto extends PartialType(CreatePokemonDto) {}
